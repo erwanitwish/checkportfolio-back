23b7fea012e7b9231647556c1946c31bda882eee
erwanitwish <quillec.erwan@gmail.com>
2020-07-29 17:40:48 +0200

    files added and deleted

---
+++ config.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+const { DB_NAME, DB_PASS, DB_USER, DB_HOST } = process.env;
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
